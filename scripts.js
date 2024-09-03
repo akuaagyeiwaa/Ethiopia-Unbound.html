e6c45c67c602e289c50020e57291f9f83fa8ffbd
AKUA AGYEIWAA A MANIESON <129429087+akuaagyeiwaa@users.noreply.github.com>
2024-09-03 11:17:08 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
+document.querySelectorAll('.personal-name').forEach(function(element) {
+    element.addEventListener('click', function() {
+        alert('This is a personal name: ' + this.innerText);
+    });
+});
 
